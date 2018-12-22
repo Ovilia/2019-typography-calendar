8a8d095d7429378563223e612c5818eb45c8f662
quillblue <sheng.qian@outlook.com>
2018-12-22 16:26:46 +0800

    Modify font day info

--- src/data/dayInfo.ts
+++ src/data/dayInfo.ts
@@ -2,35 +2,35 @@ export default `
 # 月.日 | 字体名称 | 文件名称 | 字体故事 | 备注 |
 # 可以留空行，或以 # 开头注释
 
-1.1 | Calibri | Calibri | 每一个全新的文档，都是从默认字体开始的。新的一年，想好用什么字体了吗？ |  |
+1.1 | Scriptorama Markdown JF | Scriptorama Markdown JF | 抛下一切过去的遗憾烦恼，在2019年这幅全新的画卷前开始描绘全新的图景吧。 | 生活本来就该如此简单，很多时候不需要华丽的衬线和花哨的局部变化也同样可以清晰表达一切。 |
 1.2 | Trajan | Trajan Pro 3 | 生活就像一盒 Godiva，你永远不知道下一颗是什么味道。 | Trajan 是 Godiva Logo 所使用的字体 |
 1.3 | Gloria Hallelujah | GloriaHallelujah.ttf | 某种程度上说，每个人都是字体的创作者，因为人的笔迹独一无二。 | 这款字体源于一个普通小学生的笔迹 |
 1.4 | Baskerville PT | Baskerville Display PT | 「一个人的命运，也要考虑历史的行程」| 这是一款比 Times New Roman 更古老更著名的字体。没听说过？那可能是因为它没被操作系统收入…… |
-1.5 | 思源宋体 | Source Han Serif SC | 雁北向，鹊始巢，雉始雊 | 今日小寒 |
+1.5 | 思源宋体 | Source Han Serif SC | 「雁北向，鹊始巢，雉始雊」 今日小寒 | 庞大的汉字数量使得制作中文字体的成本高昂，每一款免费中文字体都值得我们珍惜与感恩。 |
 1.6 | Bad Script | BadScript-Regular.ttf | 自黑有时候也是门艺术，靠「糟糕的手写体」这个名字成为热门字体也算是自黑的巅峰了吧？ |  |
 1.7 | Frontage Condenced Outline | FrontageCondensed-Outline.otf | 新的一周，色彩由你掌控 | 如果这是一本实体日历，我会在空心字体内涂色，你呢？ |
-1.8 | OCR B | OCR-B Std | 「两匹马屁股之间的距离，决定了现代铁路的轨距」——【待补充】 | 最早为提高机器识别准确度而设计的OCR B至今仍被用于商品条形码与信用卡。 |
+1.8 | OCR B | OCR-B Std | 两匹马屁股之间的距离，决定了现代铁路的轨距 | 最早为提高机器识别准确度而设计的OCR B至今仍被用于商品条形码与信用卡。 |
 1.9 | Carrara Light | Carrara-Light.otf | 盯着样例许久，苦于为这款字体总结一个特色。突然意识到，有时候字体少几分特色，是不是更让内容多几分出色？ |  |
-1.10 | P22 Underground | P22 Underground | 「Mind the Gap」| 1863年1月10日，世界上第一条地铁——伦敦大都会线开始运营。 |
+1.10 | P22 Underground | P22 Underground | 「Mind the Gap」——1863年1月10日，世界上第一条地铁在伦敦开始运营。 | 这是一款得到了官方授权的仿制字体，与伦敦地铁实际使用的Johnston稍有差异 |
 1.11 | Merienda | Merienda-Regular.ttf | Merienda 在西语中是下午茶点心的意思，作者希望这个字体能让大家感受到下午茶时间的愉悦——反正我是饿了。 |  |
-1.12 | Eurostile | Eurostile | 「Eurostile 热门到成为先进自行设计太空船时唯一预设字体的程度」 |  |
+1.12 | Eurostile | Eurostile | 如果说高考数学是很多人曾经的噩梦，那 Eurostile 就是许多人在这个梦中的指路明灯 | 卡西欧计算器的商标使用的就是 Eurostile |
 1.13 | Monoton | Monoton-Regular.ttf | 重要的事情要说三遍！太长了？那就在同一个字母里多写几遍吧！ | 想起了学生时代，被老师罚写字的时候，用几支笔一起抄写的经历…… |
 1.14 | Dancing Script | DancingScript-Regular.ttf | 「每一个不曾起舞的日子，都是对生命的辜负」 | 诶？尼采的这本书名字这么美，为什么豆瓣评分这么低？突然有点好奇想读一下了…… |
-1.15 | Myriad | Myriad Pro | 能够在设计上征服乔布斯的可不多，Myriad 大概算其中之一 |  |
-1.16 | City Streetwear Free | CityStreetwear-Free.otf | 谁说冬天就不能轻盈上阵？ |  |
+1.15 | Myriad | Myriad Pro | 能够在设计上征服乔布斯的可不多，Myriad 大概算其中之一 | Myraid是苹果官网使用的字体 |
+1.16 | Matrix II | Matrix II | 习惯于写四线格的欧洲抄书匠们把数字设计为高低不对齐的样式，不过把这样的字体取名为听上去就很规整的矩阵好像有一点反差萌…… |  |
 1.17 | Neue Aachen | Neue Aachen Pro | 「饿伐？要伐要帮侬叫个披萨？」 | 披萨品牌 Pa Pa Johns 使用 Aachen 作为 Logo 字体 |
 1.18 | Utopia | Utopia Std | 人无完人 |  |
 1.19 | Lobster | Lobster-Regular.ttf | 「See? He is her lobster.」| 据《老友记》中的 Phoebe 说，龙虾的配偶都是一辈子的。 |
-1.20 | Gastromond | Gastromond | 鸡始乳，鸷鸟厉疾，水泽腹坚 | 今日大寒 |
+1.20 | Gastromond | Gastromond | 「鸡始乳，鸷鸟厉疾，水泽腹坚」 今日大寒 | 同样字重下局部加粗的笔画就像厚实的棉袄一样让人觉得踏实，天寒地冻的时候也要多穿一些哦 |
 1.21 | LTC Broadway | LTC Broadway | 「City of Stars, are you shining just for me」 | 电影《La La Land》的标题用的就是这个字体，的确很「百老汇」！ |
-1.22 | DIN 1451 Pro | DIN 1451 LT Pro Engschrift | 交通领域字体设计原则：1.让司机一眼看清路牌然后专心开车，2.让监控摄像头快速识别然后准确开罚单 |  |
-1.23 | Big Caslon | Big Caslon FB | 「I'll stick with Caslon until I die.」 | ——萧伯纳 |
+1.22 | DIN 1451 Pro | DIN 1451 LT Pro Engschrift | 交通领域字体设计原则：1.让司机一眼看清路牌然后专心开车，2.让监控摄像头快速识别然后准确开罚单 | DIN 1451字体是德国公路路牌和车牌所使用的字体 |
+1.23 | Big Caslon | Big Caslon FB | 「I'll stick with Caslon until I die.」 —— 萧伯纳 | 萧伯纳是Caslon的忠实粉丝，他坚持用Caslon印刷他出版的所有作品。
 1.24 | 羡辙体 | 思源宋体 | 见字如面 |  |
 1.25 | Vollkorn | Vollkorn-Regular.ttf | Vollkorn 自 2005 年发布起就采用了当时尚属新鲜 CC 协议，相比于某些靠百年老字体躺着数钱的企业而言，可以算是业界良心了。 |  |
 1.26 | Musoe Sans | MuseoSans-500.otf | Musoe Sans 是基于因高度对称而著名的 Museo 的非衬线体。猜猜 Museo 会在日历里的哪天出现呢？ |  |
 1.27 | Versica sans | VersicaSerif.otf | 稍稍变化的笔画粗细和衬线位置，让本来看上去冰冷的印刷字体多了一分灵性 |  |
 1.28 | Balloon URW | Balloon D | 周五了，看点漫画轻松一下吧 | Balloon 是一种常见的漫画字体。 |
-1.29 | Gill Sans Nova | Gill Sans Nova | 「人们总喜欢抬头望着月亮，而忘了脚下的六便士也可以买到一本杰作。」——《月亮与六便士》 | 企鹅出版集团使用 Gill Sans 印刷他们那套经典平装图书的封面。 |
+1.29 | Gill Sans Nova | Gill Sans Nova | 人们总喜欢抬头望着月亮，而忘了脚下的六便士也可以买到一本杰作。 | 企鹅出版集团使用 Gill Sans 印刷他们那套经典平装图书的封面。 |
 1.30 | Cookie | Cookie-Regular.ttf | 忘记了之前都提到过哪些字体？你可能需要一些 Cookie！ | Cookie 一词在计算机术语里通常表示之前访问的一些记录信息。有一种说法是，这和 Cookie（曲奇饼）的共同之处在于，吃曲奇饼总会留下一些碎屑…… |
 1.31 | Sant Elia Rough-Line Two | SantEliaRough-LineTwo.otf | 淡线勾勒的草图总给人无限遐想，希望一月的 31 个小故事能让你开启对二月的期待！ |  |
 `;
