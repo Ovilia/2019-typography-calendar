a5b660716e40d1c2e530eaadb4b766aa8a8817d1
Ovilia <zwl.sjtu@gmail.com>
2019-01-05 12:12:33 +0800

    feat: 🎸 touch move pages in history page
    note that there is a bug with zrender clientToLocal, which is fixed in
    node_modules but not to git yet

--- src/pages/history/history.ts
+++ src/pages/history/history.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
+import * as zrender from 'zrender';
+
 import { StorageService } from '../../services/storage';
 import { STORE_KEY, DPR } from '../../utils/constants';
 
@@ -19,6 +21,7 @@ export class HistoryPage {
     @ViewChild('historyCanvas') historyCanvasEl: ElementRef;
     protected canvas: HTMLCanvasElement;
     protected ctx: CanvasRenderingContext2D;
+    protected zr: any;
 
     public isEmpty: boolean;
     public historyPages: any[];
@@ -42,6 +45,8 @@ export class HistoryPage {
         this.canvas.height = this.height * DPR;
         this.ctx = this.canvas.getContext('2d');
 
+        this.zr = zrender.init(this.canvas);
+
         this._init();
     }
 
@@ -60,33 +65,77 @@ export class HistoryPage {
     }
 
     protected async _render() {
-        this.ctx.shadowColor = 'rgba(0, 0, 0, 0.15)';
-        this.ctx.shadowBlur = 20;
-        this.ctx.shadowOffsetX = 2;
-        this.ctx.shadowOffsetY = 2;
-
         const imgScale = 0.8;
         const cw = this.canvas.width;
         const ch = this.canvas.height;
+
+        let touchTarget = null;
+        let z = 0;
+        let lastX;
+        let lastY;
 
         for (let i = 0; i < this.historyPages.length; ++i) {
             const page = this.historyPages[i];
 
             await new Promise((resolve, reject) => {
                 const img = new Image();
+                let zrImg;
+
                 img.onload = () => {
                     const targetWidth = img.width * imgScale;
                     const targetHeight = img.height * imgScale;
-                    const dx = (cw - targetWidth) * Math.random() * 0.5;
-                    const dy = (ch - targetHeight) * Math.random() * 0.5;
+                    const dx = (cw - targetWidth) * Math.random();
+                    const dy = (ch - targetHeight) * Math.random();
                     const rotate = (Math.random() * 2 - 1) * 15 / 180 * Math.PI;
 
-                    this.ctx.save();
-                    this.ctx.translate(cw / 2, ch / 2);
-                    this.ctx.rotate(rotate);
-                    this.ctx.translate(-targetWidth / 2, -targetHeight / 2);
-                    this.ctx.drawImage(img, dx, dy, targetWidth, targetHeight);
-                    this.ctx.restore();
+                    zrImg = new zrender.Image({
+                        style: {
+                            image: img,
+                            x: dx,
+                            y: dy,
+                            width: targetWidth,
+                            height: targetHeight,
+                            shadowBlur: 20,
+                            shadowColor: 'rgba(0, 0, 0, 0.15)',
+                            shadowOffsetX: 2,
+                            shadowOffsetY: 2
+                        },
+                        rotation: rotate,
+                        origin: [targetWidth / 2, targetHeight / 2]
+                    });
+
+                    zrImg.on('mousedown', function (e) {
+                        touchTarget = e.target;
+                        touchTarget.attr('z', ++z);
+                        lastX = e.offsetX;
+                        lastY = e.offsetY;
+                    });
+
+                    zrImg.on('mousemove', function (e) {
+                        if (!touchTarget) {
+                            return;
+                        }
+
+                        const oldStyle = touchTarget.style;
+                        const newX = oldStyle.x + (e.offsetX - lastX);
+                        const newY = oldStyle.y + (e.offsetY - lastY);
+                        touchTarget.attr({
+                            style: {
+                                x: newX,
+                                y: newY
+                            }
+                        });
+                        lastX = e.offsetX;
+                        lastY = e.offsetY;
+                    });
+
+                    zrImg.on('mouseup', function (e) {
+                        touchTarget = null;
+                        lastX = null;
+                        lastY = null;
+                    });
+
+                    this.zr.add(zrImg);
                     resolve();
                 };
                 img.onerror = reject;
