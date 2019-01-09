01ef0790b5c08a2cf8d9c8ba988679ee35a913fd
Ovilia <zwl.sjtu@gmail.com>
2019-01-09 20:20:46 +0800

    feat: 🎸 compress image

---
+++ tools/compressImg.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+imagemin ../src/assets/imgs/fonts/ui/*.png --out-dir ../src/assets/imgs/fonts/ui/
+imagemin ../src/assets/imgs/fonts/ui/dark/lunar/*.png --out-dir ../src/assets/imgs/fonts/ui/dark/lunar/
+imagemin ../src/assets/imgs/fonts/ui/dark/month/*.png --out-dir ../src/assets/imgs/fonts/ui/dark/month/
+imagemin ../src/assets/imgs/fonts/date/dark/*.png --out-dir ../src/assets/imgs/fonts/date/dark/
+imagemin ../src/assets/imgs/fonts/fontName/dark/*.png --out-dir ../src/assets/imgs/fonts/fontName/dark/
+imagemin ../src/assets/imgs/fonts/note/dark/*.png --out-dir ../src/assets/imgs/fonts/note/dark/
+imagemin ../src/assets/imgs/fonts/story/dark/*.png --out-dir ../src/assets/imgs/fonts/story/dark/
+imagemin ../src/assets/imgs/fonts/ui/dark/dayOfWeek/*.png --out-dir ../src/assets/imgs/fonts/ui/dark/dayOfWeek/
