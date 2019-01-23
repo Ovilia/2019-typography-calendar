4d103d0cbc537235ce309deef9f81be2062ff2a5
Ovilia <zwl.sjtu@gmail.com>
2019-01-23 21:54:33 +0800

    style: 💄 content from 2.1 to 2.14

--- src/data/dayInfo.ts
+++ src/data/dayInfo.ts
@@ -9,7 +9,7 @@ export default `
 1.4 | Baskerville PT | Baskerville Display PT | 一个人的命运，有时也要考虑历史的行程。| 这是一款比 Times New Roman 更古老更著名的字体。没听说过？那可能是因为它没被操作#系统收入…… |
 1.5 | 思源宋体 | Source Han Serif SC | 今日小寒。| 庞大的汉字数量使得制作中文字体的成本高昂，每一款免费中文字体都值得我们珍惜与感恩。 |
 1.6 | Bad Script | BadScript-Regular.ttf | 自黑有时候也是门艺术，靠「糟糕的手写体」这个名字成为热门字体也算是自黑的巅峰了吧？ |  |
-1.7 | Frontage Condenced Outline | FrontageCondensed-Outline.otf | 新的一周，色彩由你掌控。 | 如果这是本实体日历，我会在空心字内涂色，你呢？ |
+1.7 | Frontage#Condenced#Outline | FrontageCondensed-Outline.otf | 新的一周，色彩由你掌控。 | 如果这是本实体日历，我会在空心字内涂色，你呢？ |
 1.8 | OCR B | OCR B Std | 两匹马屁股之间的距离，决定了现代铁路的轨距。 | 最早为提高机器识别准确度而设计的 OCR B 至今仍被用于商品条形码与信用卡。 |
 1.9 | Carrara Light | Carrara-Light.otf | 盯着样例许久，苦于为这款字体总结一个特色。突然意识到，有时候字体少几分特色，是不是更让内容多几分出色？ |  |
 1.10 | P22 Underground | P22 Underground | 「Mind the Gap」#1863 年 1 月 10 日，世界上第一条地铁在伦敦开始运营。 | 这是一款得到了官方授权的仿制字体，与伦敦地铁实际使用的 Johnston 稍有差异。 |
@@ -34,25 +34,26 @@ export default `
 1.29 | Gill Sans Nova | Gill Sans Nova | 人们总喜欢抬头望着月亮，而忘了脚下的六便士也可以买到一本杰作。 | 企鹅出版集团使用 Gill Sans 印刷他们那套经典平装图书的封面，而这套书刚出版时的定价就是 6 便士一本。 |
 1.30 | Cookie | Cookie-Regular.ttf | 忘记了之前都提到过哪些字体？你可能需要一些 Cookie！ | Cookie 一词在计算机术语里通常表示之前访问的一些记录信息。有一种说法是，这和 Cookie（曲奇饼）的共同之处在于，吃曲奇饼总会留下一些碎屑…… |
 1.31 | Sant Elia Rough-Line Two | SantEliaRough-LineTwo.otf | 淡线勾勒的草图总给人无限遐想，希望一月的 31 个小故事能让你开启对二月的期待！ |  |
--->
-2.1 | Wreath Halftone | Wreath Halftone Medium.otf | 在准备新年贺卡吗？推荐一款漂亮的连体字给你！ | 在西方字体排印学中，连字（ligatures）专指两个或多个字符组合在一起时，通过调整形成整体符号的字形。 |
-2.2 | ITC American Typewriter | American Typewriter ITC Pro Bold | 「一锅火热的爱」 | 经典的「I ❤ NY」用的就是 American Typewriter。 |
+2.1 | Wreath Halftone | Wreath Halftone Medium.otf | 在准备新年贺卡吗？#推荐一款漂亮的连体字给你！ | 在西方字体排印学中，连字（ligatures）专指两个或多个字符组合在一起时，通过调整形成整体符号的字形。 |
+2.2 | Volina | volina.ttf | 设计者曾经没日没夜地舞动着她的彩带，而这些流畅优雅的曲线也留在了她设计的字体中。 |  |
 2.3 | FM Pointifax A | FontMeister - FMPointifax-A.otf | 辞旧 | 这种不仅像素低还有坏点的老式显示屏，是该进博物馆了吧？ |
-2.4 | Fabryka | 4th february - Fabryka4FMedium.otf | 名字取得巧，除夕的彩头也能抢得到 | 这款字体的出版商名字正是 2 月 4 日（February 4th）。 |
-2.5 | 喜鹊古典体 |  |  |  |
-2.7 | Oxtail | Oxtail Black | 走亲访友的时候，不妨留意一下门牌是不是这个字体呢？ | Oxtail 因其辨识度高却又不死板的特点，成为了一种不错的门牌号字体的选择。是不是特别眼熟呢？ |
-2.8 | LTC Bodoni 175 | LTC Bodoni 175 Pro Regular | 作为一直以来时尚行业的宠儿，Bodoni 字体在当今有多种复刻版本，所有版本都保留了原版极端的笔画粗细对比和尖锐的衬线。 |  |
-2.9 | Blenny Black | Blenny Black | 螺蛳壳里做道场不是一件容易的事儿，它可能是为数不多几乎不留下任何空白却依旧有相当高辨识度的字体之一。 |  |
-2.10 | Rad | Rad | 趁着假期结束前，再去冰雪世界玩一次飘移吧！ |  |
-2.11 | Tangerine | Tangerine-Regular.ttf | Tangerine 高挑优雅的上升部，宛如灵动逸荡的手写乐谱。字体设计和音乐看似风马牛不相及，实则在美学的维度上高度相关。 | 对了，某些靠行间距来卖厚书的出版社朋友们，我们的字体日历是不是启发了其中的商机？ |
-2.12 | Variex | Variex OT Light | Variex 打破了许多字体设计时的条框，用最古老的正圆弧和直线玩出了后现代的感觉。 |  |
-2.13 | PMN Caecilia | Caecilia LT Pro 55 Roman | 一款同时内含高级狗粮和精神食粮的字体。 | PMN 是设计师名字的缩写，Caecilia 是设计师妻子的名字。至于精神食粮在哪里，明天告诉你。 |
-2.14 | PMN Caecilia Sans | PMNCaeciliaSans Head Lt Italic | 显而易见，这是昨天的 PMN Caecilia 的非衬线情侣款，看电子书的朋友一定对它们很熟悉。 | PMN Caecilia 是 Kindle 使用的系统字体。 |
-2.16 | ITC Avant Garde Gothic | ITC Avant Garde Gothic Pro Book | 这款字体似乎特别受音乐厂牌们喜爱，许多古典音乐唱片的封面上都有它的影子。 |  |
-2.21 | Minion 3 | Minion 3 Medium | 开学的日子，即便已经离开象牙塔，也不妨多读两本书哦。 | Minion 最初设计时主要用来满足印刷教科书的需求。 |
+2.4 | Fabryka | 4th february - Fabryka4FMedium.otf | 名字取得巧，除夕的彩头也抢得到！ | 这款字体的出版商名字（February 4th）正是#2 月 4 日。 |
+2.5 | 喜鹊古典体 | xique-gudian.otf | 爆竹声中一岁除，春风送暖入屠苏。 | 「2019 字体日历」logo 使用的中文字体就是喜鹊古典体。新的一年，祝愿我们可以看到更多优秀的中文字体设计。 |
+2.6 | Oxtail | Oxtail OT | 走亲访友的时候，不妨留意一下门牌是不是这个字体呢？ | Oxtail 因其辨识度高却又不死板的特点，成为了一种不错的门牌号字体的选择。是不是特别眼熟呢？ |
+2.7 | ITC Avant Garde Gothic | ITC Avant Garde Gothic.ttf | 这款字体似乎深受音乐厂牌们喜爱，许多古典音乐唱片的封面上都有它的影子。 |  |
+2.8 | LTC Bodoni 175 | LTC Bodoni 175 Pro | 作为时尚行业的宠儿，Bodoni 字体有多种复刻版本，而所有版本都保留了原版极端的笔画粗细对比和尖锐的衬线。 |  |
+2.9 | Blenny Black | Blenny | 螺蛳壳里做道场不是一件容易的事，它可能是为数不多几乎不留下任何空白，却依旧有相当高辨识度的字体之一。 |  |
+2.10 | Rad | Rad Std | 趁着假期结束前，再去冰雪世界玩一次飘移吧！ |  |
+-->
+2.11 | Variex | Variex OT | Variex 打破了许多字体设计时的条#框，用最古老的正圆弧和直线玩出#了后现代的感觉。 |  |
 <!--
+2.12 | Tangerine | Tangerine-Regular.ttf | Tangerine 高挑优雅的上升部，宛如灵动逸荡的手写乐谱。字体设计和音乐看似风马牛不相及，实则在美学的维度上高度相关。 | 对了，某些靠行间距来卖厚书的出版社朋友们，我们的字体日历是不是启发了其中的商机？ |
+2.13 | PMN Caecilia | Caecilia LT Pro | 一款同时内含高级狗粮和精神食粮的字体。 | PMN 是设计师名字的缩写，Caecilia 是设计师妻子的名字。#至于精神食粮在哪里，明天告诉你。 |
+2.14 | PMN Caecilia Sans | PMN Caecilia Sans.ttf | 没错，这是昨天 PMN Caecilia 的非衬线情侣款。看电子书的朋友一定对它们很熟悉。 | PMN Caecilia 是 Kindle 使用的系统字体。 |
+<!--
+2.21 | Minion 3 | Minion 3 Medium | 开学的日子，即便已经离开象牙塔，也不妨多读两本书哦。 | Minion 最初设计时主要用来满足印刷教科书的需求。 |
 2.1 | Vectora | Vectora LT Pro 55 Roman | Vectora的小写字母比大写字母小不了多少，使得其非常适合在小字号下使用 |  |
-2.2 | Volina | Volina Regular | 她（设计者）曾经可以没日没夜地舞动她的彩带，在空中留下各式各样的轨迹。而这些流畅优雅的曲线也留在了她设计的字体中。 |  |
+2.2 | ITC American Typewriter | itc-american-typewriter.ttf | 「一锅火热的爱」 | 经典的「I ❤ NY」用的就是 American Typewriter。 |
 2.6 | JAF Domus Titling | Domus Titling Extrabold | 圆润的末端和字腔增加了几分卡通的色彩，字母 0 有没有长得像一个甜甜圈？反正我又饿了… |  |
 2.9 | Clarendon Text | Clarendon Text Pro Bold | 年初五是迎财神的日子，就选看着就多金的字体祝大家新年有财运吧。 | Clarendon 是美国富国（WELLS FARGO）银行 Logo 使用的字体。 |
 2.11 | Over the Rainbow | OvertheRainbow.ttf | 「Somewhere over the rainbow way up high There's a land that I heard of once in lullaby」 | 节后第一个工作日，听首轻松欢快的歌儿吧~ |
